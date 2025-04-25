dd9c86304c538bd89842d22a574dedb1e9f3dd9c
holyBeast <doanbanana2003@gmail.com>
2025-04-25 17:34:47 +0700

    thay bowseroute = hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,24 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { SensorProvider } from "./components/SensorContext.jsx";
+import { HashRouter } from "react-router-dom";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <BrowserRouter basename="/kma-freertos">
+//       <SensorProvider>
+//         <App />
+//       </SensorProvider>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/kma-freertos">
+    <HashRouter>
       <SensorProvider>
         <App />
       </SensorProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
